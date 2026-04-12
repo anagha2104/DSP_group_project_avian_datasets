874c9e0afd9210a7217bc71451e63c4f03c3b716
PratyushKB <pratyush.kumarbiswal@students.iiserpune.ac.in>
2026-04-12 19:30:29 +0530

    Merging

--- avian_project/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # When someone visits the base URL, load the dashboard_home view
-    path('', views.dashboard_home, name='dashboard-home'),
-    path('matcher/', views.reverse_matcher, name='reverse-matcher'),
-    path('pca/', views.pca_analysis, name='pca-analysis'),
-]
